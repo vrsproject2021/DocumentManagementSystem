235325239be863014ae22cf9acd2034e374976bc
sudhan <sudhan@DESKTOP-2HEI14A>
2021-11-18 18:19:51 -0800

    Add project files.

---
+++ DocumentManagementSystem/Controllers/HomeController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DocumentManagementSystem.Models;
+
+
+namespace DocumentManagementSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        private DbOperationDataContext _context;
+        public HomeController()
+        {
+            _context = new DbOperationDataContext();
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(Login loginObject)
+        {
+            var loginDetails = _context.tbl_Logins.Where(m => m.UserName == loginObject.UserName && m.Password == loginObject.Password).FirstOrDefault();
+
+        if (loginDetails != null)
+        {
+                var name = loginObject.UserName;
+                Session["ID"] = name;
+                if (loginDetails.Role == "admin")
+                {
+                    TempData["name"] = name;
+                    return RedirectToAction("Login", "Admin");
+                }
+                else  
+                {
+                    TempData["name"] = name;
+                    return RedirectToAction("Login", "User");
+                }
+
+
+        }
+        else
+        {
+            return View();
+        }
+          
+    }
+        public ActionResult Register()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
+
+    
---
+++ DocumentManagementSystem/Models/Login.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DocumentManagementSystem.Models
+{
+    public class Login
+    {
+        [Required]
+        [Display(Name = "UserName")]
+
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+        [Required]
+
+        //[Display(Name = "Role")]
+        //public string Role { get; set; }
+        //[Display(Name = "Remember me?")]
+        public bool RememberMe { get; set; }
+    }
+}
+    
